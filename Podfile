01b990b15618889abd9cc8d1f80deb84e7e58341
nakamura-yuta-i7 <yuta@macbook-pro.local>
2016-10-16 20:53:52 +0900

    first commit.

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'sample_swiftdate' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for sample_swiftdate
+  pod 'SwiftDate', '~> 4.0'
+  pod 'Realm', :git => 'https://github.com/realm/realm-cocoa.git', :submodules => true
+  pod 'RealmSwift', :git => 'https://github.com/realm/realm-cocoa.git', :submodules => true
+
+  target 'sample_swiftdateTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'sample_swiftdateUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- sample_swiftdate/ViewController.swift
+++ sample_swiftdate/ViewController.swift
@@ -7,19 +7,101 @@
 //
 
 import UIKit
+import SwiftDate
+import RealmSwift
 
-class ViewController: UIViewController {
+
+
+protocol AppModelProtocol {}
+extension AppModelProtocol where Self: Object {
+    func save() {
+        let realm = try! Realm()
+        try! realm.write{
+            realm.add(self, update: true)
+        }
+    }
+}
+
+
+// Dog model
+class Dog: Object, AppModelProtocol {
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    dynamic var id = NSUUID().uuidString
+    dynamic var name = ""
+    dynamic var owner: Person? // Properties can be optional
+    let owners = LinkingObjects(fromType: Person.self, property: "dogs")
+}
+
+// Person model
+class Person: Object, AppModelProtocol {
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    dynamic var id = NSUUID().uuidString
+    dynamic var name = ""
+    dynamic var birthdate = NSDate(timeIntervalSince1970: 1)
+    let dogs = List<Dog>()
+}
+
+class ViewController: UIViewController, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(NSTemporaryDirectory())
+        
+//        let date = DateInRegion() // create a date from current time in local device's region and calendar
+//        // Some examples of date as string formatted with different styles for date and time
+//        let example_1 = date.string(dateStyle: .medium, timeStyle: .long) // Sep 29, 2015, 1:01:13 PM GMT+2
+//        let example_2 = date.string(dateStyle: .short, timeStyle: .short) // 9/29/15, 1:00 PM
+//        let example_3 = date.string(dateStyle: .medium, timeStyle: .none) // September 29, 2015
+//        print(example_1)
+//        print(example_2)
+//        print(example_3)
+//        
+//        print( date.string(format: .custom("yyyy-MM-dd HH:mm:ss")) )
+//        
+//        print( NSDate() )
+//        
+//        print( DateInRegion() )
+//        
+//        print("━━━━━━━━━━━━━━━━")
+//        
+//        print( try! DateInRegion(string: "1984-05-02", format: .custom("yyyy-MM-dd")) )
+        
+//        let dog = Dog()
+//        dog.name = "ぷーちゃん"
+        
+//        let person = Person()
+//        person.name = "ゆうた"
+//        person.birthdate = try! DateInRegion(string: "1984-05-02", format: .custom("yyyy-MM-dd")).absoluteDate as NSDate
+//        person.dogs.append(dog)
+//        
+//        person.save()
+//        
+//        let pico = Person()
+//        pico.name = "ぴぃこ"
+//        pico.birthdate = try! DateInRegion(string: "1985-06-13", format: .custom("yyyy-MM-dd")).absoluteDate as NSDate
+//        pico.dogs.append(dog)
+//        
+//        pico.save()
+        
+        let realm = try! Realm()
+        
+        let yuta = realm.objects(Person.self).filter("name = 'ゆうた'").first
+        
+        print("yuta: ", yuta)
+        if ( yuta != nil ) {
+            try! realm.write {
+                realm.delete(yuta!)
+            }
+        }
+        
+        let puu = realm.objects(Dog.self).filter("id = '5DF96D79-390A-4ED3-B1C4-1C5E55B5C55C' ").last
+        
+        print("puu.owners: ", puu?.owners ?? [] )
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
